f73b772aadac5d010b2ffc43c49580cc0b994f40
KingYano <belkadi.yanis.l@gmail.com>
2024-01-06 17:52:02 +0100

    Implémentation du coverage de test

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,9 +6,14 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        provider: 'istanbul',
+        include: ["src/**/*.vue", "src/**/*.ts"],
+        exclude: ["src/main.ts", "src/**/*.spec.ts", "src/router/*"],
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      root: fileURLToPath(new URL('./', import.meta.url)),
     }
   })
 )
